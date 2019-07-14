7d9c7c55effbe21623e024b47df3ac497535baee
Alexsandro Lima <alexlima_cti@hotmail.com>
2019-07-14 20:10:08 -0300

    Implementacao Crawler parte 1

--- src/main/java/com/neoway/App.java
+++ src/main/java/com/neoway/App.java
@@ -1,13 +1,20 @@
 package com.neoway;
 
+import com.neoway.servico.FaixaCepServico;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) throws Exception {
+		
+
+		FaixaCepServico fcs = new FaixaCepServico();
+		//FaixaCepRepository fcr = new FaixaCepRepository();
+		fcs.getFaixaCep("BA");
+	
+		
+		
+	}
 }
--- src/main/java/com/neoway/servico/FaixaCepServico.java
+++ src/main/java/com/neoway/servico/FaixaCepServico.java
@@ -1,5 +1,55 @@
 package com.neoway.servico;
 
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.jsoup.Connection;
+import org.jsoup.Connection.Method;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
 public class FaixaCepServico {
+	public static final String CORREIOS_CHARSET = "ISO-8859-1";
+	List<NameValuePair> postParams;
+
+	public FaixaCepServico() {
+	}
+
+	public void getFaixaCep(String uf) throws Exception {
+		Connection conexao = Jsoup
+				.connect("http://www.buscacep.correios.com.br/sistemas/buscacep/resultadoBuscaFaixaCEP.cfm");
+		conexao.data("UF", uf);
+		conexao.data("Localidade", "");
+
+		Document documento = null;
+		try {
+			documento = Jsoup.parse(new String(conexao.method(Method.POST).execute().bodyAsBytes(),
+					FaixaCepServico.CORREIOS_CHARSET));
+		} catch (Exception e) {
+			System.out.println("Falha ao localizar faixas do estado " + uf);
+			return;
+		}
+
+		if (documento == null || documento.select("table.tmptabela").isEmpty()) {
+			return;
+		}
+		for (Element tr : documento.select("table.tmptabela tr")) {
+			if (tr.select("td").isEmpty()) {
+				continue;
+			}
+			System.out.println(tr.select("td").text());
+		}
+	}
+
+	public List<NameValuePair> getInputParams(String uf) throws UnsupportedEncodingException {
+		List<NameValuePair> paramList = new ArrayList<NameValuePair>();
+		paramList.add(new BasicNameValuePair("Localidade", ""));
+		paramList.add(new BasicNameValuePair("UF", uf));
+		return paramList;
+	}
 
 }
